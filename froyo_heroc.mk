832685aae51c1d0602a17df98cf33ba3561f29d5
Ameer Ghouse <aghouse1@gmail.com>
2010-07-09 16:19:20 -0500

    first commmit to new HeroC vendor

---
+++ froyo_heroc.mk
@@ -0,0 +1,72 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file is the build configuration for a full Android
+# build for hero hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps).
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, device/htc/heroc/device_heroc_us.mk)
+
+# Install the features available on this device.
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml
+    
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.media.dec.jpeg.memcap=10000000
+    
+PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libpath=/system/lib/libhtc_ril.so \
+    wifi.interface=tiwlan0
+    
+# Time between scans in seconds. Keep it high to minimize battery drain.
+# This only affects the case in which there are remembered access points,
+# but none are in range.
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.supplicant_scan_interval=15
+
+# density in DPI of the LCD of this board. This is used to scale the UI
+# appropriately. If this property is not defined, the default value is 160 dpi. 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=160
+    
+# Disable JIT by default
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.execution-mode=int:fast
+    
+# media configuration xml file
+PRODUCT_COPY_FILES += \
+    device/htc/heroc/media_profiles.xml:/system/etc/media_profiles.xml
+    
+## (2) Also get non-open-source aspects if available
+$(call inherit-product-if-exists, vendor/htc/dream_sapphire/dream_sapphire-vendor.mk)
+
+# stuff common to all HTC phones
+$(call inherit-product, device/htc/common/common.mk)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
+
+# Discard inherited values and use our own instead.
+PRODUCT_NAME := froyo_heroc
+PRODUCT_DEVICE := heroc
+PRODUCT_MODEL := Froyo on HeroC
